68e730b5eb056f9552e9b45db1469391e4621ea7
Delvin <delvin498@gmail.com>
2019-05-15 13:21:09 +0700

    Initial Commit

---
+++ ModellingEntity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModellingEntity
+//
+//  Created by Delvin on 15/05/19.
+//  Copyright © 2019 Delvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
